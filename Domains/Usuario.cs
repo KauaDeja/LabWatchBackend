9bc96ebcc0f83cb8902d05501fdd72d6456705a4
Lucas <lucask.silvamendes@gmail.com>
2022-05-27 14:57:26 -0300

    required na senha adicionado

--- Domains/Usuario.cs
+++ Domains/Usuario.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 
 #nullable disable
 
@@ -22,7 +24,19 @@ namespace labware_webapi.Domains
         public decimal CargaHoraria { get; set; }
         public decimal HorasTrabalhadas { get; set; }
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe a senha")]
+        [StringLength(256, MinimumLength = 8, ErrorMessage = "O campo senha precisa ter no mínimo 8 caracteres")]
+        [DataType(DataType.Password)]
         public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Por favor, confirme a senha")]
+        [StringLength(256, MinimumLength = 8, ErrorMessage = "O campo de confirmação precisa ser igual ao de senha")]
+        [DataType(DataType.Password)]
+        [Compare("Senha")]
+        public string ConfirmarSenha { get; set; }
+
+
         public string FotoUsuario { get; set; }
         public bool? Ativo { get; set; }
 
